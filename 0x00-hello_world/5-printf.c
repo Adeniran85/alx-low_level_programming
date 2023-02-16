9f15c5af1aebf27a124fc6ccb1b2a68076181e30
Your Name <you@example.com>
2023-02-16 09:53:48 -0800

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints "with proper grammar, but the outcome 
- * is a piece of art," followed by a new line
- * Return:Always 0. 
+ * main - prints "with proper grammar, but the outcome is
+ * a piece of art," followed by a new line
+ * Return: Always 0. 
  *
  */
 
@@ -11,5 +11,5 @@ int main(void)
 
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
