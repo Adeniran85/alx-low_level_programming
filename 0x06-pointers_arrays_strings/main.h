06bba56d0d7abcc2c84db246ce47f391bb40067b
Your Name <you@example.com>
2023-03-03 05:33:37 -0800

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,11 +3,16 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *s);
-char *cap_string(char *s);
-char *leet(char *s);
+char *string_toupper(char *a);
+char *cap_string(char *a);
+char *leet(char *a);
 void print_number(int n);
-char *rot13(char *);
+char *rot13(char *a);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-void _putchar.c;
+int _putchar(char c);
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#endif
