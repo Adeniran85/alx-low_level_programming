37d49548f0b9d3f4573048e37600827f88b04a50
Your Name <you@example.com>
2023-03-03 05:11:32 -0800

    9. Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,7 +4,7 @@
   * print_number - print numbers chars
   * @n: integer params
   * Return: 0
- **/
+  */
 
 void print_number(int n)
 {
@@ -17,10 +17,9 @@ void print_number(int n)
 		_putchar('-');
 		n1 = -n;
 	}
-
 	if (n1 / 10 != 0)
 	{
 		print_number(n1 / 10);
 	}
-		_putchar((n1 % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
