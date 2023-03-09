c9149924d961119e5d198db0ba22b3190447c021
Your Name <you@example.com>
2023-03-09 07:00:48 -0800

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,16 +3,16 @@
 /**
  * _puts_recursion - prints a string, followed by a new line.
  * @s: string
- * Return: Always 0 (Success).
+ * Return: no return.
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 	}
 	else
-		_putchar('\n');
+	_putchar('\n');
 }
