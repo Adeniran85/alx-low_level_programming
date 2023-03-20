0ba214972294c54ee7c8706263321eaa5319ab8e
Your Name <you@example.com>
2023-03-20 10:23:23 -0700

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -49,17 +49,17 @@ char *_strcopy(char *dest, char *src)
  *         NULL if function fails
 */
 
-dog_t *new_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner);
 {
 	dog_t *dog;
 
 	/* if name and owner are empty and age is less than zero return null*/
 	if (!name || age < 0 || !owner)
-	return (NULL);
+		return (NULL);
 
 	dog = (dog_t *) malloc(sizeof(dog_t));
 	if (dog == NULL)
-	return (NULL);
+		return (NULL);
 
 	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if ((*dog).name == NULL)
