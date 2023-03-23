419d4b39bf4465a3f86e18e8c3faf472d8580ae8
Your Name <you@example.com>
2023-03-22 17:29:19 -0700

    0. What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,10 +7,9 @@
  * @f: pointer to function
  * Return: nothing
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
-		return (0);
+		return  (0);
 	f(name);
 }
